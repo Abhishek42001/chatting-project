dff921425dc84699bd91f252426f09b6f82a57a3
Abhishek Jaiswal <84196045+Abhishek42001@users.noreply.github.com>
2021-09-22 15:49:04 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,25 +2,40 @@ import socket,pickle,ctypes,sys
 from colorama import Fore,Style,Back
 from threading import Thread
 from datetime import date, datetime
+import mysql.connector
 
-Back.BLACK
+chatdb=mysql.connector.connect(
+    host="localhost",
+    user="abhishek",
+    password="abhipc",
+    database="abhishek"
+)
+
+mycursor=chatdb.cursor()
+
 whom="all"
 flag=False
-
 diff_names=set()
 c1=socket.socket()
 print("Connecting to server...\n")
-print("Connected.\n")
 c1.connect(('localhost',9997))
+print("Connected.\n")
 
 #Taking Name
 name=input("Enter Your Name:")
 #sending the name to the server
+
+
+mycursor.execute("show tables like "+"'"+name+"'")
+
+#https://stackoverflow.com/questions/1650946/mysql-create-table-if-not-exists-error-1050/53582934
+if not mycursor.fetchall():
+    mycursor.execute("CREATE TABLE "+name+" (Who VARCHAR(255),Message VARCHAR(255))")
 c1.send(name.encode())
 
 #to listen_msg
 def listen_msg():
-    global diff_names
+    global diff_names,name
     while True:
         #it contains flag,name_list or name,msg
         l=pickle.loads(c1.recv(1024))
@@ -33,12 +48,19 @@ def listen_msg():
             for i in l[1].values():
                 diff_names.add(i)
         else:
-            name=l[1].decode()
+            recv_name=l[1].decode()
             msg=l[2].decode()
             t=datetime.now()
             time=t.strftime("%H:%M")
             #printing the recived message with time
-            print("\n"+"<"+"\033[95m{}\033[00m".format(name)+">: "+msg+"\t"+"\033[96m {}\033[00m".format(time)+"\n")
+
+            #inserting to database
+            sql = "INSERT INTO "+name+" (Who,Message) VALUES (%s, %s)"
+            val = (recv_name, msg+"    "+time)
+            mycursor.execute(sql, val)
+            chatdb.commit()
+
+            print("\n"+"<"+"\033[95m{}\033[00m".format(recv_name)+">: "+msg+"\t"+"\033[96m {}\033[00m".format(time)+"\n")
 #Receiving message from server
 print(c1.recv(1024).decode())
 print("\t",end="")
@@ -55,14 +77,13 @@ while True:
     #if we have to choose a name
     if flag:
         whom=input("Select name:")
-        #if same person is choosen e.g  aman<-->aman
+        #if same person is choosed e.g  aman<-->aman
         if whom==name:
             whom="all"
-
             print("\n"+Fore.RED+"\033[91m {}\033[00m" .format("You can't chat with yourself...\n"))
         elif whom not in diff_names:
             whom="all"
-            print("\n"+Fore.RED+"\033[91m {}\033[00m" .format("Wrong Selection\n"))
+            print("\n"+Fore.RED+"\033[91m {}\033[00m" .format("Wrong Selection(try again by using command command)\n"))
         else:
             print("\n\t"+"\033[93m {}\033[00m" .format("Go for it...")+"\n\n")
         #set flag to False so that next time it won't ask for selecting name until...
@@ -72,7 +93,7 @@ while True:
     now=datetime.now()
     time=now.strftime("%H:%M")
     if to_send=="command":
-        n=int(input("\033[92m {}\033[00m" .format("1.Quit\t2.Chat Privately 3.Chat with all:")))
+        n=int(input("\033[92m {}\033[00m" .format("1.Quit\t2.Chat Privately 3.Chat with all  4.Show history:")))
         if n==1:
             break
         elif n==2:
@@ -81,13 +102,32 @@ while True:
         elif n==3:
             whom="all"
             print("\n\t"+"\033[93m {}\033[00m" .format("Go for it...")+"\n\n")
+        elif n==4:
+            mycursor.execute("SELECT * FROM "+name)
+            myresult = mycursor.fetchall()
+            print()
+            for x in myresult:
+                for y in x:
+                    print(y,end=": ")
+                print()
+              
+            print("\n\t"+"\033[93m {}\033[00m" .format("\nThe End of history...\n\n"))
+        
         else:
             print("\033[91m {}\033[00m" .format("Invalid Choice..."))
             
     else:
         if whom!="all":
+            sql = "INSERT INTO "+name+" (Who,Message) VALUES (%s, %s)"
+            val = ("You ("+whom+")", to_send+"    "+time)
+            mycursor.execute(sql, val)
+            chatdb.commit()
             print("\033[95m{}\033[00m" .format("<You"+"("+whom+")"+">: ")+to_send+"\t"+"\033[96m {}\033[00m".format(time)+"\n")
         else:
+            sql = "INSERT INTO "+name+" (Who,Message) VALUES (%s, %s)"
+            val = ("You", to_send+"    "+time)
+            mycursor.execute(sql, val)
+            chatdb.commit()
             print("\033[95m{}\033[00m" .format("<You>: ")+to_send+"\t"+"\033[96m {}\033[00m".format(time)+"\n")
         c1.send(pickle.dumps([name.encode(),to_send.encode(),whom.encode()]))
 
